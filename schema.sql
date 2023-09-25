1852413d5cdcbbb791032e9933f87400c19869fc
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-09-25 20:06:23 +0500

    schema created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic
 
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY NOT NULL,
+    name char(35),
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
