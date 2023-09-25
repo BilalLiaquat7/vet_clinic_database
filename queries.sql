ebfc12192188e3393496b848ee371056f266bf27
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-09-26 00:01:09 +0500

    data completed and quesries completed as well

--- queries.sql
+++ queries.sql
@@ -1,3 +1,28 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT * from animals
+WHERE name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals
+WHERE 
+    date_of_birth BETWEEN '2016-01-01'
+and  '2019-01-01';
+
+Select date_of_birth from animals
+WHERE
+    name = 'Agumon' OR
+    name = 'Pikachu';
+
+SELECT name, escape_attempts from animals
+WHERE
+    weight_kg > 10.5;
+
+SELECT * from animals
+WHERE
+    neutered = TRUE;
+
+SELECT * from animals
+WHERE
+    name != 'Gabumon';
+
+SELECT * from animals
+WHERE
+    weight_kg BETWEEN 10.4 and 17.3;
